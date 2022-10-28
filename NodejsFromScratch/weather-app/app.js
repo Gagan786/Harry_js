308c5f19618aaea81814303d6b6bacf612ad2948
gagan gupta <mgagan109@gmail.com>
2022-10-28 18:08:59 +0530

    weather app geocoding

--- NodejsFromScratch/weather-app/app.js
+++ NodejsFromScratch/weather-app/app.js
@@ -1,14 +1,22 @@
-console.log("Starting");
-setTimeout(() => {
-    console.log("2 seconds"); 
-}, 2000);
-setTimeout(() => {
-    console.log("0 seconds");
-}, 0);
-console.log("fininshing");
-console.log("this is finished");
+const request = require("request");
+//request is a npm package
+const url =
+    "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=32845785ef2e9fa3d05817058690203d&units=metric";
+  //metric used here to get temperature in celcius.
+request({
+    url: url,
+    json: true,
+  },
+  (error, response) => {
+    // var data = JSON.parse(response.body); dont need to use it as json sets to true
 
-function funny() {
-    console.log('this is tocheck the stack working in node js');
-}
-funny();
+    console.log(
+      `${response.body.weather[0].description}. It is currently ${response.body.main.temp} degrees out.It feels like ${response.body.main.feels_like} degress out.`
+    );
+    })
+  
+    //geocoding
+    //adress -> lang/lon -> weather
+
+    // Goal : Print the lat/long for Los Angeles.
+    
