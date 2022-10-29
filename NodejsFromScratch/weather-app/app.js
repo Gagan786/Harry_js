9784ab2d16ed0246db2b0601878f068eaafc6aa2
gagan gupta <mgagan109@gmail.com>
2022-10-29 15:52:22 +0530

    decoding error handler

--- NodejsFromScratch/weather-app/app.js
+++ NodejsFromScratch/weather-app/app.js
@@ -10,10 +10,18 @@ request(
   },
   (error, response) => {
     // var data = JSON.parse(response.body); dont need to use it as json sets to true
-
-    console.log(
-      `${response.body.weather[0].description}. It is currently ${response.body.main.temp} degrees out.It feels like ${response.body.main.feels_like} degress out.`
-    );
+    if (error) {
+      console.log("unable to connect to Location services");;
+    }
+    else if (response.body.main == undefined) {
+      console.log('Unable to find the coordinates of location,Try another search');
+      
+    }
+    else {
+      console.log(
+        `${response.body.weather[0].description}. It is currently ${response.body.main.temp} degrees out.It feels like ${response.body.main.feels_like} degress out.`
+      );
+    }
   }
 );
 
@@ -24,8 +32,17 @@ request(
 const geoCodeURL =
   "http://api.positionstack.com/v1/forward?access_key=6b93bbab0c69fdbe22ddf6dec243894a&query=1600%20Pennsylvania%20Ave%20NW,%20Washington%20DC";
 
-request({ url: geoCodeURL, json : true }, (error, response) => {
+request({ url: geoCodeURL, json: true }, (error, response) => {
+  
+  if (error) {
+    console.log("UNable to connect to Location services");   
+  }
+  else if (response.body.data == undefined) {
+    console.log("Unable to find the location. Try another search");
+   }
+  else {
     const longitude = response.body.data[0].longitude;
     const latitude = response.body.data[0].latitude;
     console.log(`${longitude}, ${latitude}`);
+  }
 });
